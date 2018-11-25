832f97d1b4805f3a0e6c5cd577f217a3803605cc
Elizabeth Caldwell <elizabethcaldwell@Elizabeths-MacBook-Pro.local>
2018-11-24 23:48:39 -0500

    Real data from JSON

--- online-shop-app/src/App.js
+++ online-shop-app/src/App.js
@@ -1,112 +1,281 @@
-import React, { Component } from 'react';
-// import IOIcons from 'react-icons/io';
-// import logo from './logo.svg';
-// import Carousel from './Carousel'; 
+/* eslint-disable no-unused-expressions */
+import React, {Component} from 'react';
+// import IOIcons from 'react-icons/io'; 
+// import logo from './logo.svg'; import
 import './App.css';
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: []
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="row">
+                    <LeftColumn product_obj={this.state.data}/> 
+                    <RightColumn product_obj={this.state.data}/>
+                </div>
+            </div>
+        );
+    }
 
-      <div aria-label="breadcrumb">
-        <ul className="breadcrumb">
-          <li className="breadcrumb-item"><a href="#">Home</a></li>
-          <li className="breadcrumb-item active"><a href="#">Appliances</a></li>
-        </ul>
-      </div>
+    loadData() {
+        fetch('/data/item-data.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({data: data.CatalogEntryView[0]});
+                console.log(this.state.data);
+                console.log(this.state.data.Images);
+            })
+            .catch(err => console.error(this.props.url, err.toString()))
+        }
 
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-        <LeftColumn
-          product_obj={product.product_obj}
-        />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.loadData()
+    }
+
 }
 
 export default App;
 
-// function formatDate(date) {
-//   return date.toLocaleDateString();
-// }
-function LeftColumn (props){
-  return (
-    <div className="col-6">
-      <h1 className="ProductTitle">{props.product_obj.name}</h1>     
-      <ProductImage product_obj={props.product_obj}/>
-      <ProductReviews product_obj={props.product_obj}/>
-    </div>
 
-  );
+function LeftColumn(props) {
+    if (!props || props === undefined) {
+        return null;
+    } else {
+      // console.log("LeftColumn: ", props)
+        return (
+            <div className="col-6 mx-auto">
+                <h1 className="ProductTitle">{props.product_obj.title}</h1>
+                <ProductImage main_image={props.product_obj.Images} product_title={props.product_obj.title}/>
+                <ProductReviews product_reviews={props.product_obj.CustomerReview}/>
+            </div>
+
+        );
+    }
 }
 
 function ProductImage(props) {
-  return (
-    <div>
-      <img src={props.product_obj.primary_image}/>
-    </div>
-  );
+    if (!props.main_image || props.main_image === undefined) {
+        return null;
+    } else {
+        return (
+            <div>
+                <img
+                    src={props.main_image[0].PrimaryImage[0].image}
+                    alt={props.product_title}/>
+            </div>
+        );
+    }
+
 }
 
-
-function ProductReviews(props){
-  return (
-    <div>
-        <div>stars row {props.product_obj.overall_rating} <span><strong>overall</strong></span>
-        <div className="ReviewLink">
-          <a className="App-link" href="#">View all X reviews</a>
-        </div>
-        </div>
-    </div>
-  )
+function ProductReviewOverall(props) {
+    if (!props || props === undefined) {
+        return null;
+    } else {
+        return (
+            <div className="row OverallStarsRow">
+                <div className="col-6">
+                    <div>stars row {
+                            props.product_overall_rating
+                        }
+                        <span>
+                            <strong> overall</strong>
+                        </span>
+                    </div>
+                </div>
+                <div className="col-6 pull-right">
+                    <div className="ReviewLink">
+                        <a className="App-link" href="#">
+                            View all {props.product_review_count} reviews
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-// function Product(props) {
-//   return (
-//     <div className="Product">
-//       <ProductTitleImage product_obj={props.product_obj}/>
-//       <div className="Product-price">{props.product_obj.price}</div>
-//       <div className="Product-description">{props.product_obj.description}</div>
-//       <ProductReviews product_obj={props.product_obj}/>
-//     </div>
-//   );
+function ProductProReview(props) {
+    if (!props.pro || props.pro === undefined) {
+        return null;
+    } else {
+        return (
+            <div className="col-sm-5">
+                <div>
+                    <div>
+                        row of stars - {props.pro[0].overallRating}</div>
+                    <p className="card-text review-title">{props.pro[0].title}</p>
+                </div>
+                <div>
+                    <span>
+                        <small>{props.pro[0].review}</small>
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+function ProductConReview(props) {
+    if (!props.con || props.con === undefined) {
+        return null;
+    } else {
+        return (
+            <div className="col-sm-5">
+                <div>
+                    <div>
+                        row of stars - {props.con[0].overallRating}</div>
+                    <p className="card-text review-title">{props.con[0].title}</p>
+                </div>
+                <div>
+                    <span>
+                        <small>{props.con[0].review}</small>
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+function ProductReviews(props) {
+    // console.log("ProductReviews: ", props);
+    if (!props.product_reviews || props.product_reviews === undefined) {
+        return null;
+    } else {
+        return (
+            <div>
+                <ProductReviewOverall product_review_count={props.product_reviews[0].totalReviews} 
+                                      product_overall_rating={props.product_reviews[0].consolidatedOverallRating}/>
+                <div className="col-sm-10">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-sm-5">
+                                    <div>
+                                        <h6 className="card-title">PRO</h6>
+                                    </div>
+                                    <div>
+                                        <span>
+                                            <small>most helpful 4-5 star review</small>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="col-sm-5">
+                                    <div>
+                                        <h6 className="card-title">CON</h6>
+                                    </div>
+                                    <div>
+                                        <span>
+                                            <small>most helpful 1-2 star review</small>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr></hr>
+                            <div className="row">
+                                <ProductProReview pro={props.product_reviews[0].Pro}/>
+                                <ProductConReview con={props.product_reviews[0].Con}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+function RightColumn(props) {
+    // console.log("RightColumn: ", props)
+    return (
+        <div className="col-6 right-col">
+            <ProductPrice price={props.product_obj.Offers}/>
+            <hr/>
+            {/* <ProductPromotions promos={props.product_obj.Promotions}/> */}
+            <div className="row">
+              <div className="col-4">
+                <button type="button" className="btn btn-light btn-sm minor-btn">ADD TO REGISTRY</button>
+              </div>
+              <div className="col-4">
+                <button type="button" className="btn btn-light btn-sm minor-btn">ADD TO LIST</button>
+              </div>
+               <div className="col-4">
+                <button type="button" className="btn btn-light btn-sm minor-btn">SHARE</button>
+              </div>
+            </div>
+            <h1 className="ProductFeatures">Product Features</h1>
+            <ProductFeatures features={props.product_obj.ItemDescription}/>
+            {/* <div className="Product-description">{props.product_obj.description}</div> */}
+        </div>
+    );
+}
+
+function ProductPrice(props) {
+    if (!props.price || props.price === undefined) {
+        return null;
+    } else {
+        return (
+            <div className="Product-price row">
+                <h3>
+                    <strong>
+                        {props.price[0].OfferPrice[0].formattedPriceValue}
+                    </strong>
+                </h3>
+                {/*TODO: only put online tag if online price? */}
+                <span className="online-tag">
+                    <small>online price</small>
+                </span>
+            </div>
+        );
+    }
+}
+
+// function ProductPromotions(props) {
+//     if (!props.promos || props.promos === undefined) {
+//         return null;
+//     } else {
+//         const promo_list = props.promos;
+//         console.log(promo_list)
+//         var listItems = []
+//         const promoItems = promo_list.map((promo) => {
+//            listItems = promo.Description.map((numbers) => 
+//           <div>
+//             <li className="mylist">{numbers.shortDescription}</li>
+//           </div>
+//           );
+//         });
+//         console.log(promoItems)
+//         return (
+//             <div className="promos_row row">
+//                 {promoItems}
+//             </div>
+//         );
+//     }
 // }
 
-const product = {
-  product_obj: {
-    name: 'Ninja\u2122 Professional Blender with Single Serve Blending Cups',
-    primary_image: 'http://target.scene7.com/is/image/Target/14263758',
-    date: new Date(),
-    price: '$139.99',
-    description: 'For the first time EVER - you get the same professional performance power in the' +
-     'Single Serve as well as the XL 72 oz pitcher! The Ninja\u2122 Professional Blender with' +
-     'Single Serve Blending Cups allow you to crush ice into snow, blend whole fruits and vegetables ' + 
-     'into nutritious beverages, and create resort style blended drinks! Full size blender performance ' + 
-     'now in individual cups.',
-    overall_rating: 5,
-
-  },
-};
-// ReactDOM.render(
-  // <Comment
-  //   date={comment.date}
-  //   text={comment.text}
-  //   author={comment.author}
-  // />,
-//   document.getElementById('root')
-// );
+function ProductFeatures(props) {
+    if (!props.features || props.features === undefined) {
+        return null;
+    } else {
+        if (!props.features[0].features || props.features[0].features === undefined) {
+          return null;
+        } else {
+          const feature_list = props.features[0].features;
+          const listItems = feature_list.map((feature) =>
+            <li key={feature.toString()} dangerouslySetInnerHTML={{ __html: feature }}></li>
+          );
+          return (
+              <div className="promos_row row">
+                  <ul>
+                    {listItems}
+                  </ul>
+              </div>
+          );
+      }
+    }
+}
